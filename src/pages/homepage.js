b9d130ce508c8f8a9685ddfe9ac0cdc8f544ba98
TOR <52118178+fredzerpa@users.noreply.github.com>
2022-05-03 16:55:09 -0400

    Create HomePage View

---
+++ src/pages/homepage.js
@@ -0,0 +1,16 @@
+import ImageForm from "../components/image-form/image-form.component";
+
+const HomePage = () => {
+  return (
+    <div>
+      {/* Header */}
+
+      {/* Body */}
+      <ImageForm />
+
+      {/* Footer */}
+    </div>
+  );
+};
+
+export default HomePage;
