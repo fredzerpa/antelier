7f63aa333ac2782ebd2267b1269c68997af3191f
TOR <52118178+fredzerpa@users.noreply.github.com>
2022-05-05 23:28:24 -0400

    Implement tsParticles & Logo

---
+++ src/config/tsParticles/face-mask.js
@@ -0,0 +1,106 @@
+const faceMask = {
+  detectRetina: false,
+  fpsLimit: 60,
+  fullscreen: false,
+  interactivity: {
+    detectsOn: 'canvas',
+    events: {
+      onHover: {
+        enable: true,
+        mode: 'bubble',
+      },
+      resize: true,
+    },
+    modes: {
+      bubble: {
+        distance: 40,
+        duration: 2,
+        opacity: 8,
+        size: 4,
+        speed: 1,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: '#ff0000',
+      animation: {
+        enable: true,
+        speed: 5,
+        sync: true,
+      },
+    },
+    lineLinked: {
+      blink: false,
+      color: 'random',
+      consent: false,
+      distance: 30,
+      enable: true,
+      opacity: 0.3,
+      width: 0.5,
+    },
+    move: {
+      attract: {
+        enable: false,
+        rotate: {
+          x: 600,
+          y: 1200,
+        },
+      },
+      bounce: false,
+      direction: 'none',
+      enable: true,
+      outMode: 'bounce',
+      random: true,
+      speed: 0.25,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: false,
+        area: 2000,
+      },
+      limit: 0,
+      value: 300,
+    },
+    opacity: {
+      animation: {
+        enable: true,
+        minimumValue: 0.05,
+        speed: 2,
+        sync: false,
+      },
+      random: false,
+      value: 1,
+    },
+    shape: {
+      type: 'circle',
+    },
+    size: {
+      animation: {
+        enable: false,
+        minimumValue: 0.1,
+        speed: 40,
+        sync: false,
+      },
+      random: true,
+      value: 1,
+    },
+  },
+  polygon: {
+    draw: {
+      enable: true,
+      lineColor: 'rgba(255,255,255,0.2)',
+      lineWidth: 0.3,
+    },
+    move: {
+      radius: 2,
+    },
+    inlineArrangement: 'equidistant',
+    scale: 0.5,
+    type: 'inline',
+    url: '/images/tsparticles/smalldeer.svg',
+  },
+};
+
+export default faceMask;
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,15 +1,39 @@
-import ImageForm from "../components/image-form/image-form.component";
+import './homepage.styles.scss';
+
+// Particles
+import Particles from 'react-tsparticles';
+import faceMask from '../config/tsParticles/face-mask';
+import snowPreset from '../config/tsParticles/snow';
+import { loadFull } from 'tsparticles';
+
+// Components
+import ImageForm from '../components/image-form/image-form.component';
 
 const HomePage = () => {
+
+  const particlesLoaded = container => {
+    console.log(container);
+
+    // Override canvas element style
+    container.canvas.element.style.position = 'absolute';
+  };
+
   return (
-    <div>
+    <main className='homepage'>
       {/* Header */}
+      <header>
+        <Particles id='tsparticles-bg' options={snowPreset} />
+        <Particles
+          id='tsparticles-logo'
+          options={faceMask}
+          loaded={particlesLoaded}
+        />
+        <img alt='Antelier Logo' className='logo' src='logo.svg' />
+      </header>
 
-      {/* Body */}
+      {/* Face Recognition */}
       <ImageForm />
-
-      {/* Footer */}
-    </div>
+    </main>
   );
 };
 
