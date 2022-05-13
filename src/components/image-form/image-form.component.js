c0ad64a1cf19effbc807f44bfa90783cd22c18d5
TOR <52118178+fredzerpa@users.noreply.github.com>
2022-05-12 23:37:46 -0400

    Implement Image Search Validations

--- src/components/image-form/image-form.component.js
+++ src/components/image-form/image-form.component.js
@@ -1,23 +1,52 @@
 import './image-form.styles.scss';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { useForm } from 'react-hook-form';
+
+import ImageDisplayBox from '../image-display-box/image-display-box.component';
+
 const ImageForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    clearErrors,
+  } = useForm();
+
+  const onSubmit = data => console.log(data);
+
+  const handleChange = e => clearErrors(['imageUrl']);
+
   return (
-    <form className='image-form'>
+    <form className='image-form' onSubmit={handleSubmit(onSubmit)}>
       {/* Searchbar */}
       <div className='bar'>
         <input
+          {...register('imageUrl', {
+            required: true,
+            pattern:
+              // RegEx URL validation
+              /[(http(s)?)://(www.)?a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/gi,
+          })}
           className='searchbar'
           type='text'
-          title='Paste Image URL'
           placeholder='Paste Image URL'
+          onChange={handleChange}
         />
-        <img className='search-icon' alt='search icon' src='/images/components/search.svg' />
+        <button className='search-icon' type='submit'>
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </button>
       </div>
 
+      {/* React Form Validation */}
+      {(errors.imageUrl?.type === 'pattern' ||
+        errors.imageUrl?.type === 'required') && (
+        <span className='error-message'>Invalid URL</span>
+      )}
+
       {/* Image Display Box */}
-      <article className='image-display-box'>
-        
-      </article>
+      <ImageDisplayBox imageUrl={null} />
     </form>
   );
 };
