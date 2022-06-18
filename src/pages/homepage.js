17e34cbb60ff2cbeaba1adf699e040344736af11
Fred Zerpa <fredzerpa@gmail.com>
2022-06-18 01:39:25 -0400

    Remove unneeded components

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -7,16 +7,12 @@ import snowPreset from '../config/tsParticles/snow';
 // Components
 import Logo from '../components/logo/logo.component';
 import ImageDisplay from '../components/image-display/image-display.component';
-import NavigationHeader from '../components/navigation-header/navigation-header.component';
 
 const HomePage = () => {
   return (
     <main className='homepage'>
       {/* Background */}
       <Particles id='tsparticles-bg' options={snowPreset} />
-
-      {/* Navigation Section */}
-      <NavigationHeader />
 
       {/* Logo Section */}
       <Logo />
